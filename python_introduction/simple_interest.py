57d88bae28753e433417f64b2ab570e80b0bd777
Tinotenda Mjungwa <kimberlymjungwa@gmail.com>
2025-09-14 16:26:46 +0200

    simple_interest calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+P = 1000 #Principal amount (initial investment)
+R = 0.05 #Annual interest rate(as a decimal)
+T = 3 #Time money is investedfor in years
+I = P * R * T
+
+print (f" The simple interest is : {I}")
+
+
