3aee9ffc07b5632cf2a337dc1cba3bd5efbcbf29
Tinotenda Mjungwa <kimberlymjungwa@gmail.com>
2025-09-14 16:34:13 +0200

    Added the correct variable names

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,8 +1,8 @@
-P = 1000 #Principal amount (initial investment)
-R = 0.05 #Annual interest rate(as a decimal)
-T = 3 #Time money is investedfor in years
-I = P * R * T
+principal = 1000 #Principal amount (initial investment)
+rate= 0.05 #Annual interest rate(as a decimal)
+time= 3 #Time money is investedfor in years
+interest = principal * rate * time
 
-print (f" The simple interest is : {I}")
+print (f" The simple interest is : {interest}")
 
 
