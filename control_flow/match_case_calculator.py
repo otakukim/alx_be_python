5d715a87d552a348ce9839f0587f19bb73cc1533
Tinotenda Mjungwa <kimberlymjungwa@gmail.com>
2025-09-21 13:54:57 +0200

    Added folder

---
+++ control_flow/match_case_calculator.py
@@ -0,0 +1,15 @@
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the second number:"))
+operation = input("Enter operation (+, -,*, / ):")
+match operation :
+    case "+":
+        print(f"The result is {num1 + num2}")
+    case "-" : 
+        print(f"The result is {num1 - num2}")
+    case "*" :
+        print(f"The result is {num1 * num2}")
+    case "/" : 
+        if num2 != 0:
+            print(num1 / num2)
+        else:
+            print("Cannot divide by zero")
